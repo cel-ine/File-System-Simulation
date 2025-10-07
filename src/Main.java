02c80db78e3461837f3dd42ba3f71394af32f1a1
Ms. DJ <137995550+Ms-DJ-0@users.noreply.github.com>
2025-10-07 17:07:30 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,10 @@
+package src;
+
+import src.simulation.FileSystemSimulation;
+
+public class Main {
+    public static void main(String[] args) {
+        new FileSystemSimulation().run();
+    }
+}
 
