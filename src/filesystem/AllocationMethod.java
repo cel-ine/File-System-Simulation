bdb88bb62c4ef4ae0c5d25c2e5a8ec6a8a5378fd
Ms. DJ <137995550+Ms-DJ-0@users.noreply.github.com>
2025-10-10 17:47:49 +0800

    Update AllocationMethod.java

--- src/filesystem/AllocationMethod.java
+++ src/filesystem/AllocationMethod.java
@@ -1,6 +1,1 @@
-// What must exist in allocation algorithm
-public interface AllocationMethod {
-    void createFile(String name, int size, boolean[] freeBlocks);
-    void deleteFile(String name, boolean[] freeBlocks);
-    void showStatus();
-}
+
